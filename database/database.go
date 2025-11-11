aea6e338be4e91341f874660929e4a628fa8903b
mehedi <mahbub@evidentbd.com>
2025-11-11 07:00:34 +0600

    final commit

--- database/database.go
+++ database/database.go
@@ -1,7 +1,7 @@
 package database
 
 import (
-	"fmt"
+	//"fmt"
 	"log"
 	"os"
 
@@ -14,17 +14,22 @@ var DB *gorm.DB
 
 
 func ConnectDB() (*gorm.DB, error) {
-	 if err:=godotenv.Load(); err!= nil {
-		 log.Panic("Error Loading .Env File")
-	 }
-	 dbname := os.Getenv("DB_NAME")
-	 dbuser := os.Getenv("DB_USER")
-	 dbport := os.Getenv("DB_PORT")
-	 dbhost := os.Getenv("DB_HOST")
-	 dbpass := os.Getenv("DB_PASS")
+	 if err := godotenv.Load(); err != nil {
+		log.Panic("Error loading .Env File")
+	}
 
-	 dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", dbhost, dbport, dbuser, dbpass, dbname)
+	// dbname := os.Getenv("DB_NAME")
+	// dbuser := os.Getenv("DB_USER")
+	// dbport := os.Getenv("DB_PORT")
+	// dbhost := os.Getenv("DB_HOST")
+	// dbpass := os.Getenv("DB_PASS")
 
+	// dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", dbhost, dbport, dbuser, dbpass, dbname)
+	dsn:=os.Getenv("DATABASE_URL")
+
+	if dsn == "" {
+		log.Panic("Error Loading .Env File")
+	}
 	 DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
